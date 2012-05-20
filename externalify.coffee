1e618b3dce77675e27733b86bc89c7b8c14812b7
bhaskar <bhaskar@bhaskar-Inspiron-N5010.(none)>
2012-05-20 16:42:35 +0530

    modified externalify.coffee

--- externalify.coffee
+++ externalify.coffee
@@ -1,20 +1,29 @@
 (($) ->
+    
   $.fn.externalify = (options) ->
     
     settings = $.extend {
       'rel' : 'external' 
       } , options 
+     
+     
+    
+    if document.getElementById(@selector.substr(1,@selector.length))
+      if document.getElementById(@selector.substr(1,@selector.length)).nodeName is "A"
+        link = $(@selector)
+        if (link[0].toString().indexOf("http://") is 0)or(link[0].toString().indexOf("https://") is 0)
+          # "it is a single link"
+          unless link[0] is window.location.host
+            link.attr settings
+            return
     
     links = $ @selector+" a"
     if links.length is 0
-      if ((@[0].toString().indexOf("http://") is 0) or (@[0].toString().indexOf("https://") is 0))
-        unless @[0].host is  window.location.host
-          $(@).attr settings
-          
+      return
+     
     for i in links
       if (i.toString().indexOf("http://") is 0)or(i.toString().indexOf("https://") is 0)
         unless i is window.location.host
-          
           $(i).attr settings
-    
+          
 ) jQuery  
--- externalify.js
+++ externalify.js
@@ -2,16 +2,23 @@
 
   (function($) {
     return $.fn.externalify = function(options) {
-      var i, links, settings, _i, _len, _results;
+      var i, link, links, settings, _i, _len, _results;
       settings = $.extend({
         'rel': 'external'
       }, options);
-      links = $(this.selector + " a");
-      if (links.length === 0) {
-        if ((this[0].toString().indexOf("http://") === 0) || (this[0].toString().indexOf("https://") === 0)) {
-          if (this[0].host !== window.location.host) $(this).attr(settings);
+      if (document.getElementById(this.selector.substr(1, this.selector.length))) {
+        if (document.getElementById(this.selector.substr(1, this.selector.length)).nodeName === "A") {
+          link = $(this.selector);
+          if ((link[0].toString().indexOf("http://") === 0) || (link[0].toString().indexOf("https://") === 0)) {
+            if (link[0] !== window.location.host) {
+              link.attr(settings);
+              return;
+            }
+          }
         }
       }
+      links = $(this.selector + " a");
+      if (links.length === 0) return;
       _results = [];
       for (_i = 0, _len = links.length; _i < _len; _i++) {
         i = links[_i];
@@ -27,9 +34,6 @@
       }
       return _results;
     };
-    
-    
-    
   })(jQuery);
 
 }).call(this);
